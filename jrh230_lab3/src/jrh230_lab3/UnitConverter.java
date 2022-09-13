400e7e5c6402b20594e568f6a965fa60cdffaaf6
JRH230 <jrh230@pitt.edu>
2022-09-12 22:31:48 -0400

    lab3 part 1

---
+++ jrh230_lab3/src/jrh230_lab3/UnitConverter.java
@@ -0,0 +1,21 @@
+package jrh230_lab3;
+import javax.swing.JOptionPane;
+
+public class UnitConverter {
+
+	public static void main(String[] args) {
+		
+		String input = JOptionPane.showInputDialog("Enter a distance or weight amount");
+		String unit;
+		Double amount;
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
